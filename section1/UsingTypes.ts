0d8738afe4edd1dabc02c7bb242f5c04bd453cdd
shkim <rtg1539@naver.com>
2022-06-14 22:49:33 +0900

    number, string, boolean

--- section1/UsingTypes.ts
+++ section1/UsingTypes.ts
@@ -1,3 +1,4 @@
+/*
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
@@ -6,4 +7,22 @@ const number1 = 5;
 const number2 = 2.8;
 
 const result = add(number1, number2)
+console.log(result);
+*/
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+const result = add(number1, number2, printResult, resultPhrase)
 console.log(result);
